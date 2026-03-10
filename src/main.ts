c7d5ceb131c7c059cbfae0f659dd41d566df93e1
RohitS0612 <shetakeyash@gmail.com>
2026-03-10 21:51:08 +0530

    cors issue solved

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    origin: '*',
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
